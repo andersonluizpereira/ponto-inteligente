01d6d79106ceb78f79cf68f51135553fce4ec54c
andersonluizpereira <andersononus@hotmail.com>
2018-11-28 18:56:21 -0200

    Inserindo configuração de base de dados, no projeto

--- src/main/java/com/ads/pontointeligente/Application.java
+++ src/main/java/com/ads/pontointeligente/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PontoInteligenteApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PontoInteligenteApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
