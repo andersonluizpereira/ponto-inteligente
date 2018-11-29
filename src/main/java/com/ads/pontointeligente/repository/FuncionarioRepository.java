80b175f21332937bb802011c2f31aff2649cb3c8
andersonluizpereira <andersononus@hotmail.com>
2018-11-29 20:08:15 -0200

    implementando repository

---
+++ src/main/java/com/ads/pontointeligente/repository/FuncionarioRepository.java
@@ -0,0 +1,16 @@
+package com.ads.pontointeligente.repository;
+
+import com.ads.pontointeligente.entities.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Transactional(readOnly = true)
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+	Funcionario findByCpf(String cpf);
+	
+	Funcionario findByEmail(String email);
+	
+	Funcionario findByCpfOrEmail(String cpf, String email);
+}
