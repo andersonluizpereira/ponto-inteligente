48ecfb37ea71bff72c9574a0941891b57832165a
andersonluizpereira <andersononus@hotmail.com>
2018-12-05 10:59:15 -0200

    implementando token jwt

---
+++ src/main/java/com/ads/pontointeligente/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ads.pontointeligente.security.services;
+
+import java.util.Optional;
+
+import com.ads.pontointeligente.entities.Funcionario;
+import com.ads.pontointeligente.security.JwtUserFactory;
+import com.ads.pontointeligente.services.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+        if (funcionario.isPresent()) {
+            return JwtUserFactory.create(funcionario.get());
+        }
+
+        throw new UsernameNotFoundException("Email não encontrado.");
+    }
+
+}
